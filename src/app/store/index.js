592e383a6d7d6d21356af1450e2fa76d308f9a66
LionKig <kevinmorillowupwork@gmail.com>
2021-07-03 21:40:50 -0700

    202107032140

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,11 +1,43 @@
-import { createStore , applyMiddleware } from "redux";
+import { createStore , applyMiddleware , combineReducers } from "redux";
 import { defaultState } from "../../server/defaultState" ;
 import { createLogger } from "redux-logger" ;
+import createSagaMiddleWare from "redux-saga" ;
+import * as sagas from './sagas.mock' ;
+import * as mutations from './mutations' ;
+import { useStore } from "react-redux";
 
+const sagaMiddleware = createSagaMiddleWare() ; 
 
 export const store = createStore( 
-    function reducer( state = defaultState , action ){
-        return state ;
-    },
-    applyMiddleware( createLogger()), 
-)
+
+    combineReducers({
+        tasks( tasks = defaultState.tasks , action ){
+            switch( action.type ){
+                case mutations.CREATE_TASK:
+                    //console.log( action ) ; 
+                    return [...tasks, {
+                        id : action.taskID ,
+                        name : "New Task",
+                        group : action.groupID,
+                        owner : action.ownerID ,
+                        isComplete : false 
+                    }]
+            }
+            return tasks ;
+        },
+        comments( comments = defaultState.comments ){
+            return comments ;
+        },
+        groups(groups = defaultState.groups){
+            return groups ;
+        },
+        users( users = defaultState.users ){
+            return users ;
+        },
+    }),
+    applyMiddleware( createLogger() , sagaMiddleware ), 
+) ; 
+
+for( let saga in sagas ){
+    sagaMiddleware.run(sagas[saga]) ;
+}
--- src/app/store/mutations.js
+++ src/app/store/mutations.js
@@ -1,5 +1,5 @@
-export const REQUEST_TASK_CREATION = 'REQUEST_TASK_CREATION' ;
-export const CREATE_TASK = 'CREATE_TASK' ;
+export const REQUEST_TASK_CREATION = `REQUEST_TASK_CREATION` ;
+export const CREATE_TASK = `CREATE_TASK` ;
 
 export const requestTaskCreation = (groupID) => ({
     type : REQUEST_TASK_CREATION ,
